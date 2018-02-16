886199bc58feb96b2d0a1b6b13ab4512fc76a013
CZX <toczx@outlook.com>
2018-02-16 22:30:23 +0800

    done 2029 Palindromes _easy version/Palindromes _easy version.cpp

--- 2029 Palindromes _easy version/Palindromes _easy version.cpp
+++ 2029 Palindromes _easy version/Palindromes _easy version.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
 int main(){
 	int n;
@@ -6,7 +7,15 @@ int main(){
 	while(cin>>n){
 		for(int i=0;i<n;i++){
 			cin>>s;
-			
+			bool flag = true;
+			for(int j=0;j<strlen(s)/2;j++){
+				if(s[j]!=s[strlen(s)-1-j])
+					flag = false;
+			}
+			if(flag)
+				cout<<"yes"<<endl;
+			else
+				cout<<"no"<<endl;	
 		}
 	}
 	return 0;
