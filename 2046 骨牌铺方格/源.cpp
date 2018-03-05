f3a4902281bf7030c2b2a3942feeea59014e33cb
CZX <toczx@outlook.com>
2018-03-05 16:22:18 +0800

    2046

---
+++ 2046 骨牌铺方格/源.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	int a, b;
+	long long num[53];
+	num[1] = 1;
+	num[2] = 2;
+	for (int i = 3; i < 53; i++)
+		num[i] = num[i - 1] + num[i - 2];
+	
+	while (cin >> n) {
+		cout << num[n] << endl;
+	}
+	return 0;
+}
