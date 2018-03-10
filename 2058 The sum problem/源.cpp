db79b06e11d084671352c08f3cc78dbc1d00b9e5
CZX <toczx@outlook.com>
2018-03-10 19:18:04 +0800

    2058 The sum problem.lastbuildstate

--- 2058 The sum problem/源.cpp
+++ 2058 The sum problem/源.cpp
@@ -1,26 +1,20 @@
-#include<iostream>
+﻿#include<iostream>
+#include<cmath>
 using namespace std;
 int main() {
 	int N, M;
 	while (cin >> N >> M && (N != 0 || M != 0)) {
-		int mt = 0;
-		int start=1, end=0;
-		while(end<=M&&start<=M){
-			if (mt < M) {
-				end++;
-				mt += end;
-			}
-			else if (mt > M) {
-				//start++;
-				mt -= start++;
-			}
-			else {
-				printf("[%d,%d]\n", start, end);
-				end++;
-				mt += end;
-			}
+		int k = sqrt(2 * M);
+		for (int i = k; i >0&&i<=N; i--) {
+			int t = M - i * (i + 1) / 2;//t = a*i
+			if (!(t%i))
+				printf("[%d,%d]\n", t / i + 1, t / i + i);
 		}
-		cout << endl;
+		cout <<endl;
 	}
 	return 0;
 }
+
+/*
+ 
+*/
